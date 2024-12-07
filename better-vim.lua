2255460b2109400afaca48b703dd54776c1bbb10
Antunes Gabriel <gabrielantunescontato@gmail.com>
2024-12-07 19:30:02 -0300

    setup term and tabs

--- better-vim.lua
+++ better-vim.lua
@@ -16,6 +16,52 @@ return {
       end,
       opts = {}
     },
+    {
+      'akinsho/toggleterm.nvim',
+      version = "*",
+      config = function()
+        require("toggleterm").setup({
+          start_in_insert = true,  -- Abre o terminal já em modo inserção
+          toggle_in_insert = true, -- Permite togglar no modo inserção
+          direction = 'float',
+          float_opts = {
+            border = 'curved',
+            width = function() return math.floor(vim.o.columns * 0.6) end,
+            height = function() return math.floor(vim.o.lines * 0.6) end,
+          }
+        })
+      end,
+    },
+    {
+      "https://git.sr.ht/~havi/telescope-toggleterm.nvim",
+      event = "TermOpen",
+      requires = {
+        "akinsho/nvim-toggleterm.lua",
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/popup.nvim",
+        "nvim-lua/plenary.nvim",
+      },
+      config = function()
+        require("telescope").load_extension "toggleterm"
+      end,
+    },
+    {
+      "supermaven-inc/supermaven-nvim",
+      config = function()
+        require("supermaven-nvim").setup({
+          keymaps = {
+            accept_suggestion = '<C-l>', -- Aceitar a sugestão com Ctrl + l
+            clear_suggestion = '<C-h>',  -- Limpar a sugestão com Ctrl + h
+            next_suggestion = '<C-j>',   -- Próxima sugestão com Ctrl + j
+            prev_suggestion = '<C-k>',   -- Sugestão anterior com Ctrl + k
+          },
+          debounce_ms = 80,              -- Ajuste de debounce para sugestões
+          highlight = {
+            guifg = '#555555',           -- Cor da sugestão (pode personalizar)
+          },
+        })
+      end,
+    },
   },
   unload_plugins = {},
   lsps = {
@@ -95,14 +141,62 @@ return {
   },
   mappings = {
     leader = " ",
-    tabs = nil,
     custom = {},
     by_mode = {
-      n = {},
+      n = {
+        -- Abre o terminal flutuante e entra no modo inserção
+        ["<leader>tf"] = {
+          function()
+            require("toggleterm").toggle(nil, nil, nil, "float")
+          end,
+          "Toggle Floating Terminal"
+        },
+
+        -- Abre o terminal horizontal e entra no modo inserção
+        ["<leader>th"] = {
+          function()
+            require("toggleterm").toggle(nil, nil, nil, "horizontal")
+          end,
+          "Toggle Horizontal Terminal"
+        },
+
+        -- Abre o terminal vertical e entra no modo inserção
+        ["<leader>tv"] = {
+          function()
+            require("toggleterm").toggle(nil, nil, nil, "vertical")
+          end,
+          "Toggle Vertical Terminal"
+        },
+
+        -- Toggle do terminal com Ctrl + \
+        ["<C-\\>"] = {
+          function()
+            require("toggleterm").toggle()
+          end,
+          "Toggle Terminal"
+        },
+
+        -- Fecha o terminal com Ctrl + c no modo normal
+        ["<leader>tc"] = {
+          function()
+            vim.cmd("q!") -- Fecha o terminal atual
+          end,
+          "Close Terminal"
+        },
+        ["<leader>tt"] = {
+          function()
+            require("telescope").extensions.toggleterm.toggleterm()
+          end,
+          "List ToggleTerm Terminals"
+        }
+      },
       i = {},
       v = {},
       x = {},
     },
+    tabs = function(tab)
+      return "<leader>t" .. tab
+    end,
   },
   lualine = {
     options = {},
--- setup.lua
+++ setup.lua
@@ -1,15 +1,36 @@
-vim.g.clipboard = {
-  name = "WslClipboard",
-  copy = {
-    ["+"] = "clip.exe",
-    ["*"] = "clip.exe",
-  },
-  paste = {
-    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-  },
-  cache_enabled = 0,
-}
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+
+  -- Sai do terminal mode com <Esc>
+  vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], opts)
+
+  -- Toggle do terminal com Ctrl + \
+  vim.keymap.set('t', '<C-\\>', function()
+    require("toggleterm").toggle()
+  end, opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+local function is_wsl()
+  local output = vim.fn.systemlist("uname -r")[1]
+  return string.find(output:lower(), "microsoft") ~= nil
+end
+
+if is_wsl() then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
 
 require("gopher").setup {
   commands = {
@@ -42,3 +63,17 @@ require 'colorizer'.setup({
   '*',    -- Highlight all files, but customize some others.
   '!vim', -- Exclude vim from highlighting.
 }, { css_fn = true, names = true })
+
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'supermaven' }, -- Adicione Supermaven como uma fonte
+  },
+  mapping = {
+    ['<C-Space>'] = cmp.mapping.complete(),             -- Mostrar sugestões manualmente
+    ['<C-e>'] = cmp.mapping.close(),                    -- Fechar o menu de autocomplete
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),  -- Confirmar sugestão
+  },
+})
